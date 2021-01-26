9599d3f93760edb7b2e0958710735df78a321b21
Hannah <hannahgood@live.co.uk>
2021-01-26 17:49:39 +0000

    week 2 work

---
+++ CSY2043-Week-2/myfirstreact/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const myfirstelement = <p>Welcome to my first react app</p>
+
+ReactDOM.render(myfirstelement, document.getElementById('element_title'));
